163262eb16696aa89fd313bdf06a3e2f3582f43a
root <root@vps683807.ovh.net>
2019-06-07 15:04:11 +0200

    hahaha

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php session_start();?>
+<?php ob_start();?>
+<?php if (isset($_GET['page']) && file_exists("./template/".$_GET['page'].".php")): ?>
+	<?php require_once("./controller/".$_GET['page'].".php")?>
+<?php else:?>
+	<?php require_once('./template/home.php')?>
+<?php endif ?>
+<?php $content = ob_get_clean();?>
+<?php require_once('layout.php');?>
