f0698d89a7ba491f0c72cd9a1c4df18c609b0233
root <root@vps683807.ovh.net>
2019-06-07 15:57:51 +0200

    hahaha

--- layout.php
+++ layout.php
@@ -5,7 +5,14 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
      	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 	 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet" type="text/css" href="./asset/css/style.css">
     </head>
     <body>
+	<header>
+	</header>
+	<main>
+	</main>
+	<footer>
+	</footer>
     </body>
   </html>
