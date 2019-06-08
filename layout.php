1357b90b90b32efcd3127c185729fb81e9a8fd14
root <root@vps683807.ovh.net>
2019-06-08 15:30:19 +0200

    hahaha

--- layout.php
+++ layout.php
@@ -1,16 +1,30 @@
-<!DOCTYPE html>
+<!DOCTYPE htmll>
   <html>
     <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-     	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-	 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<link rel="stylesheet" type="text/css" href="./asset/css/style.css">
+	<link rel="icon" type="image/gif" href="./asset/favicon/ff.gif" />
+	<link href="https://fonts.googleapis.com/css?family=Charm" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<link rel="stylesheet"  href="./asset/css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     </head>
     <body>
-	<header>
+	<header class="flex-container">
+	 <div><a class="btn-floating btn-large red pulse"><i class="material-icons">Kamagru</i></a></div>
+	<div><a class="btn-floating btn-large red pulse"><i class="material-icons">Login</i></a></div>
+	<div><a class="btn-floating btn-large red pulse"><i class="material-icons">Register</i></a></div>
+  	<div><a class="btn-floating btn-large red pulse"><i  class="material-icons">Gallerie</i></a></div>  
 	</header>
 	<main>
+	<section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
 	</main>
 	<footer>
 	</footer>
