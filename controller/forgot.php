71e418e5e27dba9adb87f20e068cef600695944e
root <root@vps683807.ovh.net>
2019-06-12 19:44:36 +0200

    hahaha

--- controller/forgot.php
+++ controller/forgot.php
@@ -1,10 +1,15 @@
 <?php 
+
 require_once("./template/func/csrf.php");
+require_once("./config/db.php");
 
 if (!$_SESSION['co']) {
 	if ($_SERVER['REQUEST_METHOD'] == "GET") 
 		require_once("./template/forgot.php");
 	if ($_SERVER['REQUEST_METHOD'] == "POST") {
+		if (getToken("forgot")) {
+			$db = conn_db();
+		}
 	}
 } else {
 }
