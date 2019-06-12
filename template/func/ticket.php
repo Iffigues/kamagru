49902916fb5762503eb30e6d37ccbb3353783c17
root <root@vps683807.ovh.net>
2019-06-12 16:58:36 +0200

    hahaha

--- template/func/ticket.php
+++ template/func/ticket.php
@@ -9,16 +9,14 @@ function setTicket() {
 }
 
 function getTicket() {
-	if ($_COOKIE['ticket'] == $_SESSION['ticket'])
+	if ($_COOKIE['ticket'] === $_SESSION['ticket'])
 	{
 		$ticket = session_id().microtime().rand(0,9999999999);
 		$ticket = hash('sha512', $ticket);
-		$_COOKIE['ticket'] = $ticket;
+		setcookie('ticket', $ticket, time() + (60 * 20));
 		$_SESSION['ticket'] = $ticket;
 		return (1);
-	}
-	else
-	{
+	} else {
 		$_SESSION = array();
 		session_destroy();
 	}
