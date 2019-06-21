11f70825b0d87dec179ef68ea919c7d86b779032
root <root@vps683807.ovh.net>
2019-06-21 18:53:12 +0200

    hahaha

--- controller/profile.php
+++ controller/profile.php
@@ -4,33 +4,27 @@ require_once("./config/db.php");
 require_once("./template/func/passwd.php");
 
 function changeme($db, $a, $b , $c) {
-	echo "lol";
-	if ($b == $c) {
 		try {
 			$stmt = $db->prepare($a);
-			$stmt->bindParam(":login", pwd($b));
-			$stmt->bindParam(":haha", $_SESSION["login"]);
-			$stmt->execute();
+			$stmt->bindParam(":login", $b);
+			$stmt->bindParam(":haha", $c);
+			var_dump($stmt->execute());
 		} catch (PDOexception $e) {
 		}
-	}
 }
 
 function request($b, $c, $d) {
 	$db = conn_db();
 	try {
-		echo "dsddssddsdsdsdsds";
 		$stmt = $db->prepare("SELECT * FROM user WHERE login like :login LIMIT 1");
 		$stmt->bindParam(":login", $_SESSION['login']);
 		$stmt->execute();
 		$row = $stmt->fetch();
 		if ($row) {
-			if (verif_pwd($b, $row['password'])){
-				changeme($db, "UPDATE user SET password = :login WHERE login like :haha", $c, $d);
-			}
+			if (verif_pwd($b, $row['password']))
+				changeme($db, "UPDATE user SET password = :login WHERE login like :haha", pwd($c), $_SESSION['login']);
 		}
 	} catch  (PDOexception $e) {
-		echo "dsssssssssssss";
 	}
 }
 
@@ -39,10 +33,10 @@ if ($_SESSION['co']) {
 		$a = $_GET["action"];
 		if ($a == "pwd")
 			request($_POST['pwd'], $_POST['pwd1'], $_POST['pwd2']);
-		if ($a == "email")"UPDATE user SET password = :login WHERE login like :haha", $_POST['email'], $_POST['email1']);
-			changeme(conn_db(), );
+		if ($a == "email")
+			changeme(conn_db(), "UPDATE user SET email = :login WHERE login like :haha", $_POST['email1'], $_POST['email']);
 		if ($a == "login")
-		;
+			changeme(conn_db(), "UPDATE user SET login = :login WHERE login like :haha", $_POST['login1'], $_POST['login']);
 	}
 	require_once("./template/profile.php");
 }
--- template/profile.php
+++ template/profile.php
@@ -2,16 +2,16 @@
   <blockquote>
 	login   
  </blockquote>
-<form class="col s12" action="?page=login" method="POST">
+<form class="col s12" action="?page=profile&action=email" method="POST">
 	<input name="token" style="display:none;" type="text" value=<?php echo $t; ?>>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" name="mail" class="validate">
+          <input  type="email" name="email" class="validate">
         </div>
       </div>
 	<div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" name="pwd" class="validate">
+          <input  type="email" name="email1" class="validate">
         </div>
       </div>
 	<div class="row">
@@ -23,16 +23,16 @@
   <blockquote>
 	email   
  </blockquote>
-<form class="col s12" action="?page=login" method="POST">
+<form class="col s12" action="?page=profile&action=login" method="POST">
 	<input name="token" style="display:none;" type="text" value=<?php echo $t; ?>>
       <div class="row">
         <div class="input-field col s12">
-          <input id="email" type="email" name="mail" class="validate">
+	  <input type="text" name="login" class="validate">
         </div>
       </div>
 	<div class="row">
         <div class="input-field col s12">
-          <input id="password" type="password" name="pwd" class="validate">
+	  <input type="text" name="login1" class="validate">
         </div>
       </div>
 	<div class="row">
