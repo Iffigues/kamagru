47523ddb9d5fc48a8a5aad9343711bbd20868508
root <root@vps683807.ovh.net>
2019-06-23 00:07:56 +0200

    hahaha

--- template/image.php
+++ template/image.php
@@ -1,6 +1,9 @@
 <div class="container">
 	<div class="row">
-		<video class="col s9 responsive-video" id="video"></video>
+		<video id="video"></video>
+		<canvas id="canvas"></canvas>
+		<img id="photo"></img>
+	<button id="startbutton" class="btn-floating pulse"><i class="material-icons">capture</i></button>
 	</div>
 </div>
 <script type="text/javascript" src="./asset/js/video.js"></script>
