dffb468118c67c257ab9222aa82481b87fdd4dc8
root <root@vps683807.ovh.net>
2019-06-24 15:41:39 +0200

    hahaha

--- template/image.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-	<div class="row">
-		<video id="video"></video>
-		<canvas id="canvas"></canvas>
-		<img id="photo"></img>
-	<button id="startbutton" class="btn-floating pulse"><i class="material-icons">capture</i></button>
-	</div>
-</div>
-<script type="text/javascript" src="./asset/js/video.js"></script>
