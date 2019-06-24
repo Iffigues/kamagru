70fb4619a5ac43511bbdeb51a4386ddc36dd4008
root <root@vps683807.ovh.net>
2019-06-24 20:20:07 +0200

    hahaha

--- asset/js/image.js
+++ asset/js/image.js
@@ -1,13 +1,14 @@
 var img = document.getElementsByClassName("img");
 function change() {
+	var tb = ["","barbarian.png","witch_doctor.png","wizard.png","dark_cultist.png", "dune_thresher.png","gnarled_walker.png","khazra.png","undead.png"];
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
 	var image = new Image();
 	image.src = "./asset/img/icone/"+tb[parseInt(this.id)];
-	image.width = 10;
-	image.height = 10;
+	var width = image.width;
+	var height = image.height;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(image, 10, 10);
+	ctx.drawImage(image, 0, 0, width, height);
 }
 for (var i = 0; i < img.length; i++) {
 	  img[i].addEventListener("click", change, false);
