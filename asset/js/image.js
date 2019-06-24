79d69b82a9fdde8cc9984ebfb1377d7066e3b223
root <root@vps683807.ovh.net>
2019-06-25 00:26:41 +0200

    hahaha

--- asset/js/image.js
+++ asset/js/image.js
@@ -1,14 +1,51 @@
 var img = document.getElementsByClassName("img");
+
+var b = [];
+
+function getid() {
+	var i = 0;
+	var id = document.getElementsByClassName("icone");
+	for (var y = 0; y < id.length; y++) {
+		if (i < parseInt(id.id))
+			i = parseInt(id.id);
+	}
+	return (i.toString());
+}
+
+function set(e) {
+	b.push(e);
+	var id = getid();
+	var classs = "responsive-img img icone col s2 "+e.id;
+	alert(classs);
+	e.image.id = id;
+	e.image.className = classs;
+	document.getElementById("elem").appendChild(e.image);	
+}
+
+
+function bb() {
+
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	for (var i = 0; i < b.length; i++) {
+
+	}
+}
+
 function change() {
+	var tt = {};
 	var tb = ["","barbarian.png","witch_doctor.png","wizard.png","dark_cultist.png", "dune_thresher.png","gnarled_walker.png","khazra.png","undead.png"];
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
-	var image = new Image();
-	image.src = "./asset/img/icone/"+tb[parseInt(this.id)];
-	var width = image.width;
-	var height = image.height;
+	tt.image = new Image();
+	tt.image.src = "./asset/img/icone/"+tb[parseInt(this.id)];
+	tt.width = tt.image.width;
+	tt.height = tt.image.height;
+	tt.x = 0;
+	tt.y = 0;
+	tt.id = this.id;
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.drawImage(image, 0, 0, width, height);
+	ctx.drawImage(tt.image, tt.x, tt.y, tt.width, tt.height);
+	set(tt);
 }
 for (var i = 0; i < img.length; i++) {
 	  img[i].addEventListener("click", change, false);
