10a946d2d6642c4130bdb8a1c5b0b3d8ec4a189a
root <root@vps683807.ovh.net>
2019-06-25 15:04:23 +0200

    hahaha

--- asset/js/image.js
+++ asset/js/image.js
@@ -1,32 +1,97 @@
 var img = document.getElementsByClassName("img");
-
 var b = [];
+var c = {};
 
 function getid() {
 	var i = 0;
 	var id = document.getElementsByClassName("icone");
 	for (var y = 0; y < id.length; y++) {
-		if (i < parseInt(id.id))
-			i = parseInt(id.id);
+		if (i <= parseInt(id[y].id))
+			i = parseInt(id[y].id);
 	}
+	i++;
 	return (i.toString());
+}
+
+function rr(ii) {
+	var id = c.image.id;
+	for ( var i = 0; i < b.length; i++ ) {
+		if (id == b[i].image.id) {
+			if (ii == '-') {
+				if (i > 0) {
+					b[i] = b[i - 1];
+					b[i - 1] = c;
+				}
+			} 
+			if (ii == '+') {
+				if (i < b.length - 1 ) {
+					b[i] = b[i + 1];
+					b[i + 1] = c;
+				}
+			}
+			return;
+		}
+	}
+}
+
+function binding(event) {
+	if (event.key == "-") {
+		rr('-');
+		bb();
+	}
+	if (event.key == "+") {
+		rr("+");
+		bb();
+	}
+	if (event.key == "9") {
+		c.height = c.height + 1;
+		bb();
+	}
+	if (event.key == "7") {
+		c.width = c.width + 1;
+		bb();
+	}
+	if (event.key == "4") {
+		c.width = c.width - 1;
+		bb();
+	}
+	if (event.key == "6") {
+		c.height = c.height - 1;
+		bb();
+	}
+	if (event.key == "5") {
+		c.y = c.y - 1; 
+		bb();
+	}
+	if (event.key == "2") {
+		c.y = c.y + 1;
+		bb();
+	}
+	if (event.key == "1") {
+		c.x = c.x - 1;
+		bb();
+	}
+	if (event.key == "3") {
+		c.x = c.x + 1;
+		bb();
+	}
+
 }
 
 function set(e) {
 	b.push(e);
 	var id = getid();
 	var classs = "responsive-img img icone col s2 "+e.id;
-	alert(classs);
 	e.image.id = id;
 	e.image.className = classs;
-	document.getElementById("elem").appendChild(e.image);	
+	e.image.addEventListener('click', event => {c = e});
+	document.getElementById("elem").appendChild(e.image);
 	bb();
 }
 
-
 function bb() {
 	var canvas = document.getElementById("canvas");
-		var ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext("2d");
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for (var i = 0; i < b.length; i++) {
 		ctx.drawImage(b[i].image, b[i].x, b[i].y, b[i].width, b[i].height);
@@ -45,10 +110,11 @@ function change() {
 	tt.x = 0;
 	tt.y = 0;
 	tt.id = this.id;
+	c = tt;
 	set(tt);
 }
 for (var i = 0; i < img.length; i++) {
 	  img[i].addEventListener("click", change, false);
 }
 
-var tb = ["","barbarian.png","dark_cultist.png", "dune_thresher.png","gnarled_walker.png","khazra.png","	undead.png","witch_doctor.png","wizard.png"];
+document.addEventListener('keydown', (event) => {binding(event)}, false);
